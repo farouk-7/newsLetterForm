e800d480d113bbb14629d398fb6df07e1015a704
Oyedeji Opeoluwa Farouk <noblefarz7@gmail.com>
2023-08-17 01:46:50 +0100

    secondCommit

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -11,30 +11,30 @@ const MainPage = () => {
   };
 
   const handleSubmit = (e) => {
-    if(email.includes("@",".")){
-        alert("Email Successfully Submitted")
-       
-    }else{
-        alert ("Something Wrong with Email")
-        setEmail("")
-    }
     e.preventDefault();
+    if (email.includes("@", ".")) {
+      alert("Email Successfully Submitted");
+      setEmail("")
+    } else {
+      alert("Something Wrong with Email");
+      setEmail("");
+    }
   };
   return (
     <Box bg="hsl(234, 29%, 20%)" h="100vh" w="full">
-      <Flex justifyContent="center" alignContent="center" >
+      <Flex justifyContent="center" alignContent="center">
         <Flex
           bg="white"
           borderRadius="10px"
-          mt={["20px","20px","50px"]}
+          mt={["20px", "20px", "50px"]}
           p={["20px"]}
           gap="20px"
           justifyContent="space-between"
           alignItems="center"
-          flexDirection={["column-reverse","column-reverse","row"]}
+          flexDirection={["column-reverse", "column-reverse", "row"]}
         >
           <Box pl={["50px"]}>
-            <Text fontSize={["40px","40px","55px"]} fontWeight="bold">
+            <Text fontSize={["40px", "40px", "55px"]} fontWeight="bold">
               Stay updated!
             </Text>
             <Text
@@ -50,7 +50,7 @@ const MainPage = () => {
               <li>Measuring to ensure updates are a success</li>
               <li>And much more</li>
             </ul>
-            <Box my="20px" >
+            <Box my="20px">
               <FormInput
                 label={"Email Address"}
                 type={"text"}
@@ -70,7 +70,7 @@ const MainPage = () => {
               />
             </Box>
           </Box>
-          <Box >
+          <Box>
             <Image src={img} />
           </Box>
         </Flex>
